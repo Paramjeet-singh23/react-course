c4def81e3008b0544daf9f0c6aced53e2a1002c0
paramjeetsingh <Paramjeet9933@gmail.com>
2024-12-25 17:50:33 +0530

    first project completed

--- pizza-app/src/index.js
+++ pizza-app/src/index.js
@@ -51,14 +51,14 @@ const pizzaData = [
 
 function Pizza(props) {
 
-    return(<div className='pizza'>
+    return(<li className={`pizza ${props.soldOut ? "sold-out": ""}`}>
         <img src = {props.photoName} alt = {props.name} />
         <div>
             <h3>{props.name}</h3>
             <p>{props.ingredients}</p>
-            <span>{props.price}</span>
+            <span>{props.soldOut ? "Sold Out": props.price}</span>
         </div>
-    </div>);
+    </li>);
 }
 
 function Header() {
@@ -69,24 +69,28 @@ function Menu() {
     return (<main className='menu'>
         
     <h2>Our Menu</h2>
-    
-    <Pizza
-    name = "Pizza Spinaci"
-    ingredients = "Tomato, Mozarella, spinach"
-    photoName = "pizzas/spinaci.jpg"
-    price = {10} />
 
-    <Pizza 
-    name = "Pizza Salamino"
-    ingredients= "Tomato, mozarella, and pepperoni"
-    photoName= "pizzas/salamino.jpg" 
-    price= {15}/>
+    <ul className='pizzas'>
+      {pizzaData.map((pizza) => (<Pizza name={pizza.name} ingredients={pizza.ingredients} 
+      photoName={pizza.photoName} price={pizza.price} soldOut={pizza.soldOut}/>))}
+    </ul>
+    
 
     </main>);
 }
 
 function Footer() {
-    return <footer className='footer'>{new Date().toLocaleTimeString()} Fast React Pizza Co.</footer>;
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour  = 22;
+  const isOpen = hour => openHour && hour <= closeHour;
+  console.log(hour)
+
+    return <footer className='footer'>
+
+      {isOpen && ( <div className='order'> <p> We're open until {closeHour}:00. Come visit us or order online.</p><button className='btn'>Order</button></div>)}
+      
+    </footer>;
 }
 
 
